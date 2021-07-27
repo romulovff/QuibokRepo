926d3526e5cad25853a70c4d0919203e2458a235
romulovff <romulo.v.f.f@gmail.com>
2021-07-27 19:37:05 -0300

    Adding initial files

---
+++ Quibok/src/main/java/com/rome/quibok/Quibok/QuibokApplication.java
@@ -0,0 +1,13 @@
+package com.rome.quibok.Quibok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuibokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuibokApplication.class, args);
+	}
+
+}
---
+++ Quibok/src/test/java/com/rome/quibok/Quibok/QuibokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rome.quibok.Quibok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuibokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
