024a44bec266a4c8dec6f0afb2af5a8338f3155a
romulovff <romulo.v.f.f@gmail.com>
2021-08-03 20:35:12 -0300

    Creating results folder

---
+++ Quibok/results/test.java
@@ -0,0 +1,15 @@
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class test {
+
+	private value1 key1;
+	private value2 key2;
+	private value3 key3;
+
+}
